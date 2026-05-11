bc004b533beb503add22eab6d6a77ac8076e5f26
GTFoster <riverman12.foster@gmail.com>
2026-05-11 14:26:55 -0400

    huge ff changes

--- Analysis/GBIFdb_Querying.R
+++ Analysis/GBIFdb_Querying.R
@@ -1,50 +1,74 @@
 library(tidyverse)
 library(gbifdb)
 library(rgbif) #Only used to find taxonkey
+library(CoordinateCleaner)
 
-load(file="GBIFdb_query.RDA") #Load in our total species list
-gbif <- gbif_local(dir='../../../GBIF_local/occurrence/2024-04-01') #Tell R where to find our local GBIF copy
+gbif <- gbif_local(dir='/home/shared/occurrence/2024-04-01') #Tell R where to find our local GBIF copy
+#I apologize to the reproducibility gods for absolute paths
 
-nmlist <- data.frame(Name=NA, ID=1, NROW=0) #Start an empty df to keep track of>
+matches <- read.csv(file="backbone_matches.csv")
 
-for(name in unique(unlist(toquery))){ #For each species
-spdat <- gbif |> 
-  filter(species == name) |> 
-  collect() #Grab all occurence records for the species of interest and call is spdat
+nmlist <- data.frame(Name=NA, ID=0, NROW=0, time=Sys.time()) #Start an empty df to keep track of query progress
 
-
-spdat <- dplyr::filter(spdat, is.na(decimallongitude)==F)
-
-if(nrow(spdat)<1){
-  tempcheck <- data.frame(Name=name, ID=max(nmlist$ID)+1, NROW=try(nrow(spdat)))
-  nmlist <- rbind(nmlist, tempcheck)
-  write.csv(nmlist, file="Progresscheck.csv")
-  next
+keyquery <- NULL
+for(name in matches$verbatim_name){
+  filname <- name %>% tolower() %>% gsub(pattern=" ", replacement="_",.) %>%
+      paste("data/GBIF/occs/", ., ".csv", sep="") #Create a filename
+    if(file.exists(filname)==TRUE){ #If the occ file already exists locally and is filled, skip and move to the next species
+      if(file.info(filname)$size > 150){
+      next
+      }
+    }
+  key <- matches$speciesKey[matches$verbatim_name==name] #Save the key to search by
+  keyquery <- rbind(keyquery, data.frame(name=name, key=key))
 }
 
-filname <- name %>% tolower() %>% gsub(pattern=" ", replacement="_",.) %>%
-  paste("data/GBIF/occs/", ., ".csv", sep="") #Create a filename
+#Each query seems to take ~8min, pretty much reguardless of size. So to speed things up, we're going to call our gbifdb query in chunks and then split the outputs up afterwards
+idex <- c(0, seq(from=1, to=nrow(keyquery), by=30)[-1], nrow(keyquery))
 
-spdat <- spdat %>% #Do some data cleaning
-  dplyr::filter(occurrencestatus  == "PRESENT") %>%
-  dplyr::filter(!basisofrecord %in% c("FOSSIL_SPECIMEN")) %>% #Remove fossils
-  CoordinateCleaner::cc_cen( #Remove records within 1k buffer of country cent>
-    lon = "decimallongitude",
-    lat = "decimallatitude",
-    buffer = 1000, # radius of circle around centroid to look for centroids
-    value = "clean",
-    test="both")  %>%
-  cc_sea( #Remove oceanic records
-    lon = "decimallongitude",
-    lat = "decimallatitude"
-  )# %>%
-  #dplyr::filter(., establishmentMeans != "introduced") #Remove known introduced  spp
-
-occs <- dplyr::select(spdat, genus, species,scientificname, decimallatitude, decimallongitude, elevation, day, month, year, taxonkey) %>% unique()
-write.csv(occs, file=filname, row.names = FALSE)
-
-tempcheck <- data.frame(Name=name, ID=max(nmlist$ID)+1, NROW=try(nrow(occs)))
-nmlist <- rbind(nmlist, tempcheck)
-gc()
-write.csv(nmlist, file="Progresscheck.csv")
+for(j in idex[-length(idex)]){
+  keystemp <- keyquery$key[(j+1):(j+30)]
+  Sys.time()
+    spdat <- gbif |> 
+      #filter(taxonkey %in% key) |> 
+      filter(taxonkey %in% keystemp) |> 
+      collect() #Grab all occurence records for the species of interest and call is spdat, using our backbone key to search
+    Sys.time()
+    outrun <- spdat
+    spdat <- dplyr::filter(spdat, is.na(decimallongitude)==F) #remove recs without latlong
+    
+    spdat <- spdat %>% #Do some data cleaning
+        dplyr::filter(occurrencestatus  == "PRESENT") %>%
+        dplyr::filter(!basisofrecord %in% c("FOSSIL_SPECIMEN")) %>% #Remove fossils
+        CoordinateCleaner::cc_cen( #Remove records within 1km buffer of country centroids
+        lon = "decimallongitude", 
+        lat = "decimallatitude", 
+        buffer = 1000, # radius of circle around centroid to look for centroids
+        value = "clean",
+        test="both")  %>% 
+        cc_sea( #Remove oceanic records
+      lon = "decimallongitude",
+      lat = "decimallatitude"
+      )# %>%
+      #dplyr::filter(., establishmentMeans != "introduced") #Remove known introduced  spp
+    
+    occs <- dplyr::select(spdat, genus, species,scientificname, decimallatitude, decimallongitude, elevation, day, month, year, taxonkey) %>% unique() 
+    #subset across species and save into seperate files
+    
+    for(i in (j+1):(j+30)){
+      filname <- keyquery$name[i] %>% tolower() %>% gsub(pattern=" ", replacement="_",.) %>%
+      paste("data/GBIF/occs/", ., ".csv", sep="") #Create a filename
+      tempocc <- dplyr::filter(occs, taxonkey==keyquery$key[i])
+      write.csv(tempocc, file=filname, row.names = FALSE) 
+      IDkey <- matches$verbatim_index[matches$verbatim_name==keyquery$name[i]]
+      if(length(IDkey)==0){
+        IDkey <- NA
+      }
+      tempcheck <- data.frame(Name=keyquery$name[i], ID=IDkey, NROW=try(nrow(tempocc)), time=Sys.time())
+    }
+    
+    nmlist <- rbind(nmlist, tempcheck)
+    rm(occs, tempocc)
+    gc()
+    write.csv(nmlist, file="Progresscheck.csv")
 }
